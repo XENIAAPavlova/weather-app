6c11c415143cf38a4208d60f257d30f2ba1c086b
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-02-03 16:34:13 +0100

    Init project

---
+++ src/weather.js
@@ -0,0 +1,60 @@
+
+// let now = new Date();
+
+// let days = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+
+// let currentDay = days[now.getDay()];
+// console.log(currentDay);
+
+// let currentHours = now.getHours();
+// console.log(currentHours);
+
+// let currentMinutes = now.getMinutes();
+// console.log(currentMinutes);
+
+// let todays = `${currentDay}, ${currentHours} : ${currentMinutes}`;
+// console.log(todays);
+
+// function formateTime(actualTime) {
+//   alert("");
+// }
+// let element = document.querySelector("#current-time");
+// console.log(element);
+
+// element.innerHTML = todays;
+
+// function search(event) {
+//   event.preventDefault();
+//   let searchInput = document.querySelector("#search-city-input");
+//   let cityElement = document.querySelector("#city-name");
+//   cityElement.innerHTML = searchInput.value;
+// }
+
+// let form = document.querySelector("#submit-button");
+// form.addEventListener("submit", search);
+
+// function convertToF(event) {
+//   event.preventDefault();
+//   let changeMetric = document.querySelector("#see-temperature");
+//   changeMetric.innerHTML = "59°";
+// }
+
+// let clickOnF = document.querySelector("#number-fahrenheit");
+// clickOnF.addEventListener("click", convertToF);
+
+// function convertToC(event) {
+//   event.preventDefault();
+//   let changeMetrics = document.querySelector("#see-temperature");
+//   changeMetrics.innerHTML = "15°";
+// }
+
+// let clickOnC = document.querySelector("#number-celsius");
+// clickOnC.addEventListener("click", convertToC);
---
+++ src/weatherAPI.js
@@ -0,0 +1,66 @@
+let apiKey = "dcfba56f795371b9b39f70882499c480";
+// let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=${apiKey}&units=metric`;
+
+console.log("Hello");
+
+// 4. инфо передаеттся в open weather
+// 5. open weather ищет инфо о погоде
+
+function returnLocation(response) {
+  // 6. программа получает инфо от OW
+  let showWeather = document.querySelector("#see-temperature");
+  let temperature = Math.round(response.data.main.temp);
+  showWeather.innerHTML = `${temperature}°`;
+
+  let showCity = document.querySelector("#city-name");
+  showCity.innerHTML = response.data.name;
+
+  let displayCurrentCity = document.querySelector("#search-city-input");
+  displayCurrentCity.value = response.data.name;
+
+  let showHumidity = document.querySelector("#humidity-percentages");
+  showHumidity.innerHTML = `${response.data.main.humidity}%`;
+
+  let showWind = document.querySelector("#wind-indicator");
+  showWind.innerHTML = `${response.data.wind.speed} km/h`;
+
+  let showWeatherDescription = document.querySelector("#weather-description");
+  showWeatherDescription.innerHTML = response.data.weather[0].description;
+}
+
+// 1. Пользователь вводит город в input
+// 2. пронрамма достает значение, которые ввел пользователь
+
+function enterTheCity(event) {
+  event.preventDefault();
+  let locationInput = document.querySelector("#search-city-input");
+
+  // 3. программа ищет путь для передачи запроса в OW
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(returnLocation);
+}
+
+let submitButton = document.querySelector("#search-city");
+submitButton.addEventListener("click", enterTheCity);
+
+/*
+ *
+ */
+
+function retrievePosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(returnLocation);
+  console.log(position);
+}
+
+function clickCurrent(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let currentButton = document.querySelector("#current-city");
+currentButton.addEventListener("click", clickCurrent);
+
+
